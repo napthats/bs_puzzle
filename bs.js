fbfd2762c60fef8661716c4e8337d37de4e5e2b7
Naoki Yoshinaga <nap525@gmail.com>
2015-03-07 12:03:24 +0900

    Add some keyboard shortcuts.

--- bs.js
+++ bs.js
@@ -16,16 +16,40 @@ $(document).ready(function() {
     $(document).keydown(function(event) {
         switch (event.which) {
             case 38:
+            case 75:
+            case 87:
                 game.move(0);
                 break;
             case 39:
+            case 76:
+            case 68:
                 game.move(1);
                 break;
             case 40:
+            case 74:
+            case 83:
                 game.move(2);
                 break;
             case 37:
+            case 72:
+            case 65:
                 game.move(3);
+                break;
+            case 48:
+                $('#board_id').val('boardX');
+                restart();
+                break;
+            case 49:
+                $('#board_id').val('board1');
+                restart();
+                break;
+            case 50:
+                $('#board_id').val('board2');
+                restart();
+                break;
+            case 51:
+                $('#board_id').val('board3');
+                restart();
                 break;
             case 82:
                 restart();
